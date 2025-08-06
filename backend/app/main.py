48f265d82a5991d905e45b06dbf0dc6bfd32ba5d
ferreiraluc <l.adrianobf@gmail.com>
2025-08-06 14:36:12 -0300

    added render.yaml production stats

--- backend/app/main.py
+++ backend/app/main.py
@@ -39,7 +39,12 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://localhost:5173", 
+        "http://127.0.0.1:3000",
+        "https://erp-eleven-frontend.onrender.com"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
