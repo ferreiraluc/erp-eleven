068d95dcc6955feddf3a24b392728ef7496e92d9
ferreiraluc <l.adrianobf@gmail.com>
2026-05-03 23:48:49 -0300

    update

--- backend/app/main.py
+++ backend/app/main.py
@@ -140,7 +140,7 @@ app.add_middleware(
         "http://localhost:5173"
     ],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allow_headers=["Authorization", "Content-Type", "Accept", "Origin", "X-Requested-With", "Access-Control-Allow-Origin"],
 )
 
